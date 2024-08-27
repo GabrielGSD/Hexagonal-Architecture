3c4b807cfb6c489328abb2f8857e6e810f7661c0
GabrielGSD <gabrieldaniel@gec.inatel.br>
2024-08-26 21:13:18 -0300

    feat: Implement ProductService methods for CRUD operations

---
+++ application/product_test.go
@@ -0,0 +1,60 @@
+package application_test
+
+import (
+	"testing"
+
+	"github.com/gabrielgsd/go-hexagonal/application"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestProduct_Enable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	err := product.Enable()
+	require.Nil(t, err)
+
+	product.Price = 0
+	err = product.Enable()
+	require.Equal(t, "The price must be greater than zero to enable the product", err.Error())
+}
+
+func TestProduct_Disable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.ENABLED
+	product.Price = 10
+
+	err := product.Disable()
+	require.Equal(t, "The price must be zero to disable the product", err.Error())
+
+	product.Price = 0
+	err = product.Disable()
+	require.Nil(t, err)
+}
+
+func TestProduct_IsValid(t *testing.T) {
+	product := application.Product{}
+	product.ID = uuid.New().String()
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	_, err := product.IsValid()
+	require.Nil(t, err)
+
+	product.Status = "INVALID"
+	_, err = product.IsValid()
+	require.Equal(t, "The status must be enabled or disabled", err.Error())
+
+	product.Status = application.ENABLED
+	_, err = product.IsValid()
+	require.Nil(t, err)
+
+	product.Price = -10
+	_, err = product.IsValid()
+	require.Equal(t, "The price must be greater than zero", err.Error())
+}
