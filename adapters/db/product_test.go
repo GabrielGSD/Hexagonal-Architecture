0e02c449015e864b3d936c32fac46a91ee8b3117
GabrielGSD <gabrieldaniel@gec.inatel.br>
2024-08-26 22:38:10 -0300

    test: Add unit tests for Save method in ProductDb

--- adapters/db/product_test.go
+++ adapters/db/product_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/gabrielgsd/go-hexagonal/adapters/db"
+	"github.com/gabrielgsd/go-hexagonal/application"
 	"github.com/stretchr/testify/require"
 )
 
@@ -51,4 +52,25 @@ func TestProductDb_Get(t *testing.T) {
 	require.Equal(t, "Product 1", product.GetName())
 	require.Equal(t, 10.0, product.GetPrice())
 	require.Equal(t, "disabled", product.GetStatus())
+}
+
+func TestProductDb_Save(t *testing.T) {
+	setUp()
+	defer Db.Close()
+	productDb := db.NewProductDb(Db)
+
+	product := application.NewProduct()
+	product.Name = "Product 1"
+	product.Price = 20
+
+	productResult, err := productDb.Save(product)
+	require.Nil(t, err)
+	require.Equal(t, product.Name, productResult.GetName())
+	require.Equal(t, product.Price, productResult.GetPrice())
+	require.Equal(t, product.Status, productResult.GetStatus())
+
+	product.Status = "enabled"
+	productResult, err = productDb.Save(product)
+	require.Nil(t, err)
+	require.Equal(t, product.Status, productResult.GetStatus())
 }
